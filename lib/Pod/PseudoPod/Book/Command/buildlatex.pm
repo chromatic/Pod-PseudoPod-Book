e3c6f604c53fa471d9a5a6cc4d2a29faa00ff059
chromatic <chromatic@wgz.org>
2011-11-28 16:42:26 -0800

    Added buildlatex command.

---
+++ lib/Pod/PseudoPod/Book/Command/buildlatex.pm
@@ -0,0 +1,37 @@
+package Pod::PseudoPod::Book::Command::buildlatex;
+# ABSTRACT: command module for C<ppbook buildlatex>
+
+use parent 'Pod::PseudoPod::Book::Command';
+
+use strict;
+use warnings;
+
+use autodie;
+use Path::Class;
+use File::Basename;
+use Pod::PseudoPod::DOM::App::ToLaTeX;
+
+sub execute
+{
+    my ($self, $opt, $args) = @_;
+    my $output_dir          = dir( qw( build latex ) );
+
+    my %files = map
+    {
+        my $file     = $_;
+        my $filename = fileparse( $file, qr{\..*} ) . '.pod';
+        my $outfile  = file( $output_dir, $filename );
+
+        $file => $outfile,
+    } get_chapter_list();
+
+    Pod::PseudoPod::DOM::App::ToLaTeX::process_files_with_output( %files );
+}
+
+sub get_chapter_list
+{
+    my $glob_path = file( qw( build chapters chapter_??.pod ) );
+    return glob "$glob_path";
+}
+
+1;
