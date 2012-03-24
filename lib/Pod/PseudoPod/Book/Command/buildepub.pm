c1f2f1ed6e7f78aa3ecfb79ec276b5e48a25b39f
chromatic <chromatic@wgz.org>
2012-03-24 12:09:59 -0700

    Switched to XHTML 1.1 doctype.

--- lib/Pod/PseudoPod/Book/Command/buildepub.pm
+++ lib/Pod/PseudoPod/Book/Command/buildepub.pm
@@ -197,10 +197,9 @@ sub add_cover
     # Add an additional cover page for other eBook readers.
     my $cover_xhtml = <<END_XHTML;
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <title></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
